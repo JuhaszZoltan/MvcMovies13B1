bbcdf8e5ca72197823c1495ed96426acaa60895f
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2025-09-18 11:35:18 +0200

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovies.Controllers;
+
+public class HelloWorldController : Controller
+{
+
+    public string Index()
+    {
+        return "ez az alapértelmezett action";
+    }
+
+    public string Welcome()
+    {
+        return "ez pedig a Welcome action method...";
+    }
+}
